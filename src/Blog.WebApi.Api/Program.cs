5b4bba97b8ee1a51b9249e79a9b2e68441558e7f
Guilherme Navarro <guilherme-navarro@outlook.com>
2023-07-04 19:29:44 -0300

    ADD: Created AuthController, User entity and added Jwt's middleware at Startup

--- src/Blog.WebApi.Api/Program.cs
+++ src/Blog.WebApi.Api/Program.cs
@@ -1,14 +1,42 @@
 
 using Blog.WebApi.Infra;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
 
+builder.Services.AddCors();
+
+builder.Services.AddAuthentication(_ =>
+{
+    _.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    _.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    _.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(_ =>
+{
+    // Como vai ser validado a chave
+    _.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true, 
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddControllers();
 
-var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+var connection = config["ConnectionStrings:DefaultConnection"];
 builder.Services.AddDbContext<BlogContext>(options =>
 {
     options.UseNpgsql(connection,
@@ -30,6 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ src/Blog.WebApi.Domain/Models/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.WebApi.Domain.Models.Entities
+{
+    public sealed class User : Base
+    {
+        public string Email  { get; private set; }
+        public string Password { get; private set; }
+    }
+}
